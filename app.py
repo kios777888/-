9774a0e396e0b3e3622a9b086e1ff45e61f0cab0
kios777888 <thmanbwsaht@gmail.com>
2025-11-24 20:46:47 +0100

    remove venv from repo

--- app.py
+++ app.py
@@ -1036,3 +1036,6 @@ if __name__ == '__main__':
         debug=os.environ.get('FLASK_ENV') != 'production',
         allow_unsafe_werkzeug=True
     )
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
